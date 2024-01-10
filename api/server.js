3b144f0d3568fa124496952d8b9dda85960fe783
Marlon Colato <mcolato2104@gmail.com>
2024-01-09 23:21:25 -0500

    server.get by Id is working

--- api/server.js
+++ api/server.js
@@ -13,10 +13,32 @@ server.get('/', (req, res) => {
 });
 
 server.get('/api/users', async (req, res) => {
-    try{
+    try {
         const users = await Users.find();
-        res.status(200).json(users)
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: `The users information could not be retrieved`});
+    };
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const users = await Users.findById(id);
+        if (!users) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" });
+        }else{
+            res.status(200).json(users);
+        }
+    } catch (err) {
+        res.status(500).json({ message: `The users information could not be retrieved` });
+    }
+})
+
+server.post('/api/users', async (req, res) => {
+    try{
+
     }catch(err){
-        res.status(500).json({message: `The users information could not be retrieved`})
+        res.status(500).json({message: "There was an error while saving the user to the database"})
     }
 })
