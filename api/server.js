7f68021faf373a9c2366164adef896eea1660c47
Marlon Colato <mcolato2104@gmail.com>
2024-01-09 22:31:02 -0500

    succesful get users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+server.get('/api/users', async (req, res) => {
+    try{
+        const users = await Users.find();
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json({message: `The users information could not be retrieved`})
+    }
+})
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./api/server');
 
-const port = 9000;
+const port = 8000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`server started on port ${port}`);
+})
