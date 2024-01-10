bc6a4c7c0691500a5b1e108cc2fda81899e9ef95
Marlon Colato <mcolato2104@gmail.com>
2024-01-09 23:59:23 -0500

    server.delete is passing !!

--- api/server.js
+++ api/server.js
@@ -25,6 +25,7 @@ server.get('/api/users/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const users = await Users.findById(id);
+        
         if (!users) {
             res.status(404).json({ message: "The user with the specified ID does not exist" });
         } else {
@@ -38,6 +39,7 @@ server.get('/api/users/:id', async (req, res) => {
 server.post('/api/users', async (req, res) => {
     try {
         const { name, bio } = req.body;
+        
         if (!name || !bio) {
             res.status(400).json({ message: "Please provide name and bio for the user" });
         } else {
@@ -53,6 +55,7 @@ server.put('/api/users/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const { name, bio } = req.body;
+       
         if (!name || !bio) {
             res.status(400).json({ message: "Please provide name and bio for the user" })
         } else {
@@ -66,5 +69,20 @@ server.put('/api/users/:id', async (req, res) => {
         }
     } catch (err) {
         res.status(500).json({ message: "The user information could not be modified" });
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await Users.remove(id);
+        if (!deletedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" });
+        } else {
+            res.json(deletedUser);
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: "The user could not be removed" });
     }
 })
